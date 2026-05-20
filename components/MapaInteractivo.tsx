cb025b091b1411daefc9b9d2b0d42fbde406b367
pat0corbalan <patocorbalan99@gmail.com>
2026-05-20 08:45:00 -0300

    correccion bug map interactivo

--- components/MapaInteractivo.tsx
+++ components/MapaInteractivo.tsx
@@ -1,79 +1,12 @@
 "use client"
 
-import { useEffect, useMemo } from "react"
-import { MapContainer, TileLayer, Marker, useMap, useMapEvents } from "react-leaflet"
-import L from "leaflet"
+import dynamic from "next/dynamic"
 
-// Solución definitiva al bug de iconos rotos de Leaflet en Next.js
-const iconDefault = L.icon({
-  iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
-  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-})
-L.Marker.prototype.options.icon = iconDefault
+const MapaInteractivoClient = dynamic(
+  () => import("./MapaInteractivoClient"),
+  {
+    ssr: false,
+  }
+)
 
-interface MapaInteractivoProps {
-  lat: number
-  lng: number
-  onChangeCoords: (lat: string, lng: string) => void
-}
-
-// Subcomponente para mover el mapa programáticamente cuando cambian los inputs
-function ActualizarVistaMapa({ center }: { center: [number, number] }) {
-  const map = useMap()
-  useEffect(() => {
-    map.setView(center, map.getZoom())
-  }, [center, map])
-  return null
-}
-
-// Subcomponente para capturar clicks en cualquier coordenada libre del mapa
-function ManejadorClicks({ onClick }: { onClick: (lat: number, lng: number) => void }) {
-  useMapEvents({
-    click(e) {
-      onClick(e.latlng.lat, e.latlng.lng)
-    },
-  })
-  return null
-}
-
-export default function MapaInteractivo({ lat, lng, onChangeCoords }: MapaInteractivoProps) {
-  // Memorizamos el centro para evitar re-renders cíclicos innecesarios
-  const center: [number, number] = useMemo(() => [lat, lng], [lat, lng])
-
-  return (
-    <div className="w-full h-64 rounded-xl overflow-hidden border border-border relative z-0">
-      <MapContainer 
-        center={center} 
-        zoom={13} 
-        scrollWheelZoom={false} 
-        className="w-full h-full"
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        
-        <ActualizarVistaMapa center={center} />
-        
-        <ManejadorClicks 
-          onClick={(newLat, newLng) => onChangeCoords(newLat.toFixed(6), newLng.toFixed(6))} 
-        />
-        
-        <Marker 
-          position={center} 
-          draggable={true}
-          eventHandlers={{
-            dragend: (e) => {
-              const marker = e.target
-              const position = marker.getLatLng()
-              onChangeCoords(position.lat.toFixed(6), position.lng.toFixed(6))
-            }
-          }}
-        />
-      </MapContainer>
-    </div>
-  )
-}
+export default MapaInteractivoClient
